4dfc1820f1d5b79197e2ddba742859cce54c568e
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-09-12 17:41:12 -0600

    updated app.js routes for React due to version use

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './Header';
-import Navigation from './Navigation';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 function App() {
   return (
@@ -9,15 +13,12 @@ function App() {
       <div className="App">
         <Header />
         <Navigation />
-        {/* Define your routes and components here */}
-        <Switch>
-          {/* Add routes for About Me, Portfolio, Contact, and Resume */}
-          <Route path="/" exact component={AboutMe} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/resume" component={Resume} />
-
-        </Switch>
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
       </div>
     </Router>
   );
