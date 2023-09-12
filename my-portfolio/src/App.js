5a61e5176006cb4e6708e957460dab879511f572
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-09-12 17:11:55 -0600

    Package.json updated and basic setup for App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Navigation />
+        {/* Define your routes and components here */}
+        <Switch>
+          {/* Add routes for About Me, Portfolio, Contact, and Resume */}
+          <Route path="/" exact component={AboutMe} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
