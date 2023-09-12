e244432033f7bb80628e933593ad53dcc726d153
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-09-12 16:54:35 -0600

    Basic Structure sent up and Header.js

---
+++ my-portfolio/src/pages/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Holden Melander</h1>
+      {/* Navigation links will go here */}
+    </header>
+  );
+}
+
+export default Header;
