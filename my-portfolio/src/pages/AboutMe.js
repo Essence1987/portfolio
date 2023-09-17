7355c6b371dee5c1bafad1b25617f95eb3fd1588
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-09-17 11:55:23 -0600

    Added a sekfpic and edited styling for aboutme

--- my-portfolio/src/pages/AboutMe.js
+++ my-portfolio/src/pages/AboutMe.js
@@ -5,22 +5,39 @@ function AboutMe() {
     return (
         <div className="about-me">
             <h2>About Me</h2>
-            <img src="placeholder.jpg" alt="Holden Melander" />
-            <p>
-                Hello! I'm Holden Melander, a web developer with a diverse background that combines my experience in retail management and my passion for web development. At 36 years old, I bring a wealth of real-world experience to the world of programming.
+            <div className="image-card">
+                <img src="/selfpic.jpeg" alt="Holden Melander" />
+            </div>
+            <div className="text-card">
+                <p>
+                     I'm Holden Melander, a web developer with a diverse background that combines my experience in retail management and my passion for web development. At 36 years old, I bring a wealth of real-world experience to the world of programming.
+                </p>
 
-                My journey into web development has been anything but typical. After graduating from Brigham Young University-Idaho in 2014 with a degree in Business Management and Entrepreneurial Emphasis, I embarked on a successful career in retail management.
+                <p>
+                    My journey into web development has been anything but typical. After graduating from Brigham Young University-Idaho in 2014 with a degree in Business Management and Entrepreneurial Emphasis, I embarked on a successful career in retail management.
+                </p>
 
-                I worked in the retail industry for over a decade, starting at 21. I held managerial positions at well-known companies like Walmart and 2020 Events, where I honed my leadership and problem-solving skills. I also had the privilege of owning my own successful retail business for the past two years, where I learned valuable lessons in entrepreneurship and customer service.
+                <p>
+                    I worked in the retail industry for over a decade, starting at 21. I held managerial positions at well-known companies like Walmart and 2020 Events, where I honed my leadership and problem-solving skills. I also had the privilege of owning my own successful retail business for the past two years, where I learned valuable lessons in entrepreneurship and customer service.
+                </p>
 
-                Only this year, in 2023, I made a bold decision to venture into web development. I enrolled in college to pursue my passion for coding, and I've been captivated by the world of web development ever since.
+                <p>
+                    Only this year, in 2023, I made a bold decision to venture into web development. I enrolled in college to pursue my passion for coding, and I've been captivated by the world of web development ever since.
+                </p>
 
-                As I immerse myself in coding, I'm discovering the incredible power of technology to create meaningful change. I've gained proficiency in JavaScript, HTML, CSS, and other web development tools, and I'm eager to apply these skills to build innovative web solutions.
+                <p>
+                    As I immerse myself in coding, I'm discovering the incredible power of technology to create meaningful change. I've gained proficiency in JavaScript, HTML, CSS, and other web development tools, and I'm eager to apply these skills to build innovative web solutions.
+                </p>
 
-                Beyond coding, I have a passion for a wide range of interests. I'm an avid reader of fantasy novels, a travel enthusiast who loves exploring new places, a board and video game aficionado, an outdoors enthusiast who enjoys fishing, and a writer of short stories. In my spare time, I often craft imaginative worlds and characters through my writing.
+                <p>
+                    Beyond coding, I have a passion for a wide range of interests. I'm an avid reader of fantasy novels, a travel enthusiast who loves exploring new places, a board and video game aficionado, an outdoors enthusiast who enjoys fishing, and a writer of short stories. In my spare time, I often craft imaginative worlds and characters through my writing.
+                </p>
 
-                As a web developer, I'm excited to combine my diverse background and technical expertise to craft engaging and user-friendly web applications. Let's connect and embark on this exciting journey together!
-            </p>
+                <p>
+                    As a web developer, I'm excited to combine my diverse background and technical expertise to craft engaging and user-friendly web applications. Let's connect and embark on this exciting journey together!
+                </p>
+
+            </div>
         </div>
     );
 }
